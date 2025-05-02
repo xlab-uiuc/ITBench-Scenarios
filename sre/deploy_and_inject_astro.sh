6c6e0efa2a02421b3c60897cdcd57bce368c6a9d
Yiming Su <yiming34@illinois.edu>
2025-05-01 20:18:10 -0500

    added streamlined scripts

---
+++ sre/deploy_and_inject_astro.sh
@@ -0,0 +1,6 @@
+source /home/yiming34/tianyin_group/xlab-itbench/sre/venv/bin/activate
+cd /home/yiming34/tianyin_group/xlab-itbench/sre
+which python3
+make deploy_observability_stack
+make deploy_astronomy_shop
+INCIDENT_NUMBER=$1 make inject_incident_fault
---
+++ sre/start_provider.sh
@@ -0,0 +1,2 @@
+cd /home/yiming34/tianyin_group/xlab-itbench/sre/local_cluster
+make start_provider
